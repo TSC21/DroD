fe1876a23bb77b3c99dd4ff531f0039458eb990c
Satya Abhiram Theli <satyatabhiram@gmail.com>
2015-12-21 02:17:23 +0530

    Update LearningPhase.py
    Defined LearningPhase class. New function to return cluster centers instead of block-based image

--- LearningPhase.py
+++ LearningPhase.py
@@ -3,35 +3,48 @@ import numpy
 import median
 import blockBasedModel
 
-video = cv2.VideoCapture('D:\Stuff\Drowning Detection\drowning.mp4')
+name = 'LearningPhase'
 
-retVal, firstFrame = video.read()
-resizedFirstFrame = cv2.resize(firstFrame, None, fx = 0.5, fy = 0.5, interpolation = cv2.INTER_LINEAR)
-
-buffer = [resizedFirstFrame] #creating an array of images
-
-cv2.namedWindow('video')
-for count in xrange(99): #considering 100 frames to find initial background
-    retVal, frame = video.read()
-    if (not retVal) or (cv2.waitKey(1)==27): #break if frame is not read
-        break
+class LearningPhase:
     
-    resizedFrame = cv2.resize(frame, None, fx = 0.5, fy = 0.5, interpolation = cv2.INTER_LINEAR) #resize each frame
-    buffer.append(resizedFrame) #buffer is made by appending 99 frames to the initial first frame
+    def learn(self, video, nSamples):
+
+        retVal, firstFrame = video.read()
+        resizedFirstFrame = cv2.resize(firstFrame, None, fx = 0.5, fy = 0.5, interpolation = cv2.INTER_LINEAR)
+
+        buffer = [resizedFirstFrame] #creating an array of images
+
+        cv2.namedWindow('video')
+        for count in xrange(nSamples-1): #considering nSamples frames to find initial background
+            retVal, frame = video.read()
+            if (not retVal) or (cv2.waitKey(1)==27): #break if frame is not read
+                break
     
-    cv2.imshow('video', resizedFrame)
+            resizedFrame = cv2.resize(frame, None, fx = 0.5, fy = 0.5, interpolation = cv2.INTER_LINEAR) #resize each frame
 
-cv2.destroyWindow('video')
-video.release()
+            buffer.append(resizedFrame) #buffer is made by appending nSamples-1 frames to the initial first frame
+    
+            cv2.imshow('video', resizedFrame)
 
-print median.name
-medianImage = median.getMedian(buffer) #function gives median of buffer
+        cv2.destroyWindow('video')
+        video.release()
 
-print blockBasedModel.name
-clusteredMedianImage = blockBasedModel.getBlockBasedModel(medianImage, 40) #takes image, size of square block, returns k-means clustered-block based-model
+        print median.name
+        medianObj = median.Median()
+        initialBackground = medianObj.getMedian(buffer) #function gives median of buffer
 
-cv2.imshow('m', medianImage)
-cv2.imshow('cm', clusteredMedianImage)
-cv2.waitKey(0)
+        print blockBasedModel.name
+        blockBasedModelObj = blockBasedModel.BlockBasedModel()
+        blockBasedInitialBackground = blockBasedModelObj.getBlockBasedModel(initialBackground, 40) #takes image, size of square block, returns k-means clustered-block based-model
+        self.initialBackgroundClusterCenters = blockBasedModelObj.getClusterCenters()
 
-cv2.destroyAllWindows()
+        cv2.imshow('m', initialBackground)
+        cv2.imshow('cm', blockBasedInitialBackground)
+        cv2.waitKey(0)
+
+        cv2.destroyAllWindows()
+
+        return blockBasedInitialBackground
+
+    def getInitialBackgroundClusterCenters(self):
+        return self.initialBackgroundClusterCenters
