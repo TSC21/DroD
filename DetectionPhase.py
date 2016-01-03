31b19824beb33d02d3eec11bf4fed7d0790f892b
Satya Abhiram Theli <satyatabhiram@gmail.com>
2016-01-03 19:23:55 +0530

    Update DetectionPhase.py
    Added HysteresisThreshold Implementation

--- DetectionPhase.py
+++ DetectionPhase.py
@@ -8,12 +8,21 @@ class DetectionPhase:
     
     def detect(self, video, initialBackgroundClusterCenters):
         cv2.namedWindow('DifferenceImage')
+        cv2.namedWindow('SegmentedImage')
+        cv2.namedWindow('UpdatedBackground')
 
         print colorDiscrepancy.name
         colorDiscrepancyObj = colorDiscrepancy.ColorDiscrepancy()
 
         backgroundClusterCenters = initialBackgroundClusterCenters
 
+        buffer = [] #creating an array of images
+
+        meanObj = mean.Mean()
+
+        blockBasedModelObj = blockBasedModel.BlockBasedModel()
+        hysteresisThresholdObj = hysteresisThreshold.HysteresisThreshold()
+        
         while(video.isOpened()):
             retVal, frame = video.read()
             if (not retVal) or (cv2.waitKey(1)==27): #break if frame is not read
@@ -23,7 +32,22 @@ class DetectionPhase:
 
             differenceImage = colorDiscrepancyObj.getColorDiscrepancy(resizedFrame, backgroundClusterCenters, 40)
 
+            segmentedImage = hysteresisThresholdObj.hysThreshold(differenceImage, 127, 127);
+            
+            buffer.append(resizedFrame)
+            if(len(buffer)>10):
+                buffer.pop(0)
+            updatedBackground = meanObj.getMean(buffer)
+
+            blockBasedUpdatedBackground = blockBasedModelObj.getBlockBasedModel(updatedBackground, 40)
+            backgroundClusterCenters = blockBasedModelObj.getClusterCenters()
+
             cv2.imshow('DifferenceImage', differenceImage)
+            cv2.imshow('SegmentedImage', segmentedImage)
+            cv2.imshow('UpdatedBackground', updatedBackground)
 
         cv2.destroyWindow('DifferenceImage')
+        cv2.destroyWindow('SegmentedImage')
+        cv2.destroyWindow('UpdatedBackground')
+        cv2.destroyAllWindows()
         video.release()
