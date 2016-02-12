ecf99417381ebd4b4bdaf624487b648778ded562
TSC21 <n.marques21@hotmail.com>
2016-02-12 11:59:39 +0000

    update mean

--- scripts/mean.py
+++ scripts/mean.py
@@ -1,20 +1,25 @@
+#
+# Copyright (c) 2016, DroD Team.
+# All rights reserved.
+#
+
 import cv2
-import numpy
+import numpy as np
 
 name = 'mean'
+
 
 class Mean:
 
     def getMean(self, buffer):
-        
-        stackedFlattened = numpy.vstack((frame.ravel() for frame in buffer)) 
+        stackedFlattened = np.vstack((frame.ravel() for frame in buffer))
         firstFrame = buffer[0]
         del buffer
-        
-        meanImageFlattened = numpy.mean(stackedFlattened, axis = 0)
+
+        meanImageFlattened = np.mean(stackedFlattened, axis=0)
         del stackedFlattened
-        
-        meanImage = numpy.uint8(meanImageFlattened.reshape(firstFrame.shape))
+
+        meanImage = np.uint8(meanImageFlattened.reshape(firstFrame.shape))
         del meanImageFlattened
 
         return meanImage
